65e9ffc43f81803f81cf417b043da28e257a8653
Tegar Ibrahim <57317804+xkyrage@users.noreply.github.com>
2024-02-09 18:21:12 +0700

    adding some script for modal

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
+"use strict";
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+};
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+};
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+/**
+ * header & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
 // Modal Box
 const itemDetailModal = document.querySelector("#item-detail-modal");
-const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+const itemDetailButtons = document.querySelectorAll("#item-detail-button");
 
 itemDetailButtons.forEach((btn) => {
   btn.onclick = (e) => {
@@ -9,13 +62,13 @@ itemDetailButtons.forEach((btn) => {
   };
 });
 
-// klik tombol close modal
+// close modal
 document.querySelector(".modal .close-icon").onclick = (e) => {
   itemDetailModal.style.display = "none";
   e.preventDefault();
 };
 
-// klik di luar modal
+// outside modal
 window.onclick = (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = "none";
